f02b6810dc072e8b493b14d8f21329424448422b
Dru Mumba <drusademumba@gmail.com>
2023-04-04 07:34:04 +0300

    Add array of cards and card timer

--- CardWorkout/CardSelectionVC.swift
+++ CardWorkout/CardSelectionVC.swift
@@ -11,23 +11,34 @@ class CardSelectionVC: UIViewController {
     
     @IBOutlet var CardImageView: UIImageView!
     @IBOutlet var buttons: [UIButton]!
-    
-    
+    var timer: Timer!
+    var cards: [UIImage] = Card.allValues
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        startTimer()
         
+    }
+    
+    func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 0.3, target: self, selector: #selector(showRandomCard), userInfo: nil, repeats: true)
+    }
+    
+    @objc func showRandomCard(){
+        CardImageView.image = cards.randomElement() ?? UIImage (named: "AS")
     }
     
     @IBAction func stopButtonTapped(_ sender: UIButton) {
-        
+        print("Stopped")
     }
     
     @IBAction func restartButtonTapped(_ sender: UIButton) {
+        print("Restart")
     }
     
     
     @IBAction func rulesButtonTapped(_ sender: UIButton) {
+        print("Rules")
     }
 }
 
