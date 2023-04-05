4b11cd13e4a48258aa19306264ed3959f449cfe4
Dru Mumba <drusademumba@gmail.com>
2023-04-05 14:34:50 +0300

    Add card assets

---
+++ CardWorkout2/CardSelectionVC.swift
@@ -0,0 +1,18 @@
+//
+//  CardSelectionVC.swift
+//  CardWorkout2
+//
+//  Created by Dru Mumba on 05/04/2023.
+//
+
+import UIKit
+
+class CardSelectionVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .green
+    }
+    
+}
